c127efda4161f12a763a664c8ddb6e6ce1c7e95f
malinxiao <malinxiao@hotmail.com>
2018-03-08 16:28:13 +0000

    Check in Power BI QnA Bot

---
+++ Power BI QnA Bot/PowerBIQnA/dataninjago.bot.powerbiQnA/Dialogs/PowerBIQnADialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System.Configuration;
+
+namespace dataninjago.bot.powerbiQnA.Dialogs
+{
+    [Serializable]
+    public class PowerBIQnADialog : QnAMakerDialog
+    {
+        public PowerBIQnADialog():base(
+            new QnAMakerService(
+                new QnAMakerAttribute(
+                    ConfigurationManager.AppSettings["QnaSubscriptionKey"], 
+                    ConfigurationManager.AppSettings["QnaKnowledgebaseId"],
+                    "Sorry, couldn't find anything, please rephrase your question", 
+                    0.5)
+                )
+            )
+        {
+        }
+
+    }
+}
